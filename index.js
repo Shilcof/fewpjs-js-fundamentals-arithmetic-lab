28dfbfcd3b855e235e2ee471c7480db84b233d2d
Shilcof <robert.shilcof@gmail.com>
2021-01-29 15:35:27 +0000

    commit tree 054e1a5eae787e594e17ce024819064dab339938

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31, num2 = 2, multiply = num1 * num2;
 
-// Write your code here
+const random = Math.ceil(Math.random()*100);
+
+const num3 = 459, num4 = 5;
+
+const mod = num3 % num4;
+
+const numberSet = [1,2,3,4,20];
+
+const max = Math.max(...numberSet);
